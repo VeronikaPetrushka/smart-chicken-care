14b1dabf5fc159cdb6293188239e2d25b7b784c6
Veronika <veronikapetrushka@gmail.com>
2025-04-24 12:15:31 +0200

    new loaders

--- App.jsx
+++ App.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Animated, Dimensions } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -22,17 +24,101 @@ enableScreens();
 
 const Stack = createStackNavigator();
 
+const appLoaders = [
+    require('./App/sourceAssets/appLoaders/chickenLoader1.png'),
+    require('./App/sourceAssets/appLoaders/chickenLoader2.png')
+]
+
 const App = () => {
+      const [currentLoader, setCurrentLoader] = useState(0);
+      const slideAnimation1 = useRef(new Animated.Value(0)).current;
+      const slideAnimation2 = useRef(new Animated.Value(Dimensions.get('window').width)).current;
+
+      useEffect(() => {
+            const animationTimeout = setTimeout(() => {
+            slideToNextLoader();
+      }, 1500);
+
+      const navigation = setTimeout(() => {
+            navigateToMenu();
+            }, 4000);
+
+            return () => {
+                  clearTimeout(animationTimeout);
+                  clearTimeout(navigation);
+            };
+      }, []);
+
+      const slideToNextLoader = () => {
+            Animated.parallel([
+            Animated.timing(slideAnimation1, {
+                  toValue: -Dimensions.get('window').width,
+                  duration: 1500,
+                  useNativeDriver: true,
+            }),
+            Animated.timing(slideAnimation2, {
+                  toValue: 0,
+                  duration: 1500,
+                  useNativeDriver: true,
+                  }),
+            ]).start(() => {
+                  setCurrentLoader(1);
+            });
+      };
+
+      const navigateToMenu = () => {
+            setCurrentLoader(2);
+      };  
 
   return (
       <MusicProvider>
             <NavigationContainer>
-                  <Stack.Navigator initialRouteName={"AppAnimationRoute" }>    
-                        <Stack.Screen 
-                              name="AppAnimationRoute" 
-                              component={AppAnimationRoute} 
-                              options={{ headerShown: false }} 
-                        />
+                  <Stack.Navigator
+                        screenOptions={{
+                        headerShown: false,
+                        animation: 'fade',
+                        animationDuration: 1000,
+                  }}>
+                        {currentLoader < 2 ? (
+                              <Stack.Screen name="Welcome" options={{ headerShown: false }}>
+                              {() => (
+                              <View style={{ flex: 1, backgroundColor: '#000' }}>
+                                    <Animated.Image
+                                          source={appLoaders[0]}
+                                          style={[
+                                          { 
+                                                width: '100%', 
+                                                height: '100%', 
+                                                position: 'absolute',
+                                          },
+                                          { 
+                                                transform: [{ translateX: slideAnimation1 }],
+                                          },
+                                          ]}
+                                    />
+                                    <Animated.Image
+                                          source={appLoaders[1]}
+                                          style={[
+                                          { 
+                                                width: '100%', 
+                                                height: '100%', 
+                                                position: 'absolute',
+                                          },
+                                          { 
+                                                transform: [{ translateX: slideAnimation2 }],
+                                          },
+                                          ]}
+                                    />
+                              </View>
+                              )}
+                              </Stack.Screen>
+                        ) : (
+                              <Stack.Screen 
+                                    name="AppAnimationRoute" 
+                                    component={AppAnimationRoute} 
+                                    options={{ headerShown: false }} 
+                              />
+                        )}         
                         <Stack.Screen 
                               name="ChickenBookRoute" 
                               component={ChickenBookRoute} 
